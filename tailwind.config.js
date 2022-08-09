6ceca5b59e744766e3f723a204616265ee86ade0
behnam <salehinezhad.b@gmail.com>
2022-08-09 05:21:18 +0430

    add extend colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          500: '#888888',
+          600: '#999999',
+          700: '#EAEAEA',
+          800: '#FAFAFA',
+          900: '#FFFFFF'
+        },
+        secondary: {
+          500: '#666666',
+          600: '#444444',
+          700: '#333333',
+          800: '#111111',
+          900: '#000000'
+        }
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
