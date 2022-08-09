4ec5326096d45b6668f0263f120a2daaa5cbc926
behnam <salehinezhad.b@gmail.com>
2022-08-09 04:50:20 +0430

    update prettier config

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: [require('prettier-plugin-tailwindcss')],
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  tabWidth: 2,
+  useTabs: false,
+  arrowParens: 'avoid',
+  printWidth: 80
+}
