7437421f77037b80100d587886d9bf08cb7d9d54
behnam <salehinezhad.b@gmail.com>
2022-08-11 06:11:07 +0430

    add link

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -8,19 +8,19 @@ import { NavBarItem } from '@/components/NavBar/NavBarItem'
 export const NavBar = () => {
   return (
     <div className="fixed bottom-0 z-20 flex w-full justify-between p-3 lg:hidden">
-      <NavBarItem>
+      <NavBarItem href="/">
         <HomeIcon className="h-8" />
       </NavBarItem>
-      <NavBarItem>
+      <NavBarItem href="/search">
         <SearchIcon className="h-8" />
       </NavBarItem>
-      <NavBarItem>
+      <NavBarItem href="/">
         <PlusIcon className="h-8" />
       </NavBarItem>
-      <NavBarItem>
+      <NavBarItem href="/">
         <BellIcon className="h-8" />
       </NavBarItem>
-      <NavBarItem>
+      <NavBarItem href="/">
         <UserIcon className="h-8" />
       </NavBarItem>
     </div>
--- components/NavBar/NavBarItem.tsx
+++ components/NavBar/NavBarItem.tsx
@@ -1,11 +1,16 @@
+import Link from 'next/link'
+
 interface Props {
+  href: string
   children: React.ReactNode
 }
 
-export const NavBarItem = ({ children }: Props) => {
+export const NavBarItem = ({ href, children }: Props) => {
   return (
-    <div className="fill-secondary-900 transition-colors duration-300 dark:fill-primary-900">
-      {children}
-    </div>
+    <Link href={href}>
+      <a className="fill-secondary-900 transition-colors duration-300 dark:fill-primary-900">
+        {children}
+      </a>
+    </Link>
   )
 }
