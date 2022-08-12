dda5a87fbe9f6f9b3137f7297530af8293920e80
behnam <salehinezhad.b@gmail.com>
2022-08-12 05:49:17 +0430

    export Author

---
+++ components/Author.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+
+import { UserIcon } from '@/components/icons/UserIcon'
+import { useAuthor } from '@/hooks/useAuthor'
+
+interface Props {
+  id: number
+}
+export const Author = ({ id }: Props) => {
+  const { author, isLoading } = useAuthor(id)
+
+  if (isLoading) return <div>loading...</div>
+
+  return (
+    <div className="flex items-center">
+      <Link href={`/users/${author.username}`}>
+        <a className="mr-2 flex h-12 w-12 items-center justify-center rounded-full bg-secondary-900 transition-colors duration-300 dark:bg-primary-900">
+          <UserIcon className="w-5 fill-primary-900 transition-colors duration-300 dark:fill-secondary-900" />
+        </a>
+      </Link>
+      <div className="flex flex-col">
+        <Link href={`/users/${author.username}`}>
+          <a>{author.name}</a>
+        </Link>
+        <Link href={`/users/${author.username}`}>
+          <a className="text-xs text-primary-600">{`@${author.username}`}</a>
+        </Link>
+      </div>
+    </div>
+  )
+}
--- components/Post.tsx
+++ components/Post.tsx
@@ -1,9 +1,8 @@
 import Link from 'next/link'
 
+import { Author } from '@/components/Author'
 import { CommentIcon } from '@/components/icons/CommentIcon'
 import { LikeIcon } from '@/components/icons/LikeIcon'
-import { UserIcon } from '@/components/icons/UserIcon'
-import { useAuthor } from '@/hooks/useAuthor'
 import type { PostModel } from '@/models/post.model'
 import { dateTimeDistanceFilter } from '@/utils/date'
 
@@ -34,31 +33,5 @@ export const Post = ({ post }: Props) => {
         <div className="ml-2">{post.likes.length}</div>
       </div>
     </article>
-  )
-}
-interface AuthorProps {
-  id: number
-}
-const Author = ({ id }: AuthorProps) => {
-  const { author, isLoading } = useAuthor(id)
-
-  if (isLoading) return <div>loading...</div>
-
-  return (
-    <div className="flex items-center">
-      <Link href={`/users/${author.username}`}>
-        <a className="mr-2 flex h-12 w-12 items-center justify-center rounded-full bg-secondary-900 transition-colors duration-300 dark:bg-primary-900">
-          <UserIcon className="w-5 fill-primary-900 transition-colors duration-300 dark:fill-secondary-900" />
-        </a>
-      </Link>
-      <div className="flex flex-col">
-        <Link href={`/users/${author.username}`}>
-          <a>{author.name}</a>
-        </Link>
-        <Link href={`/users/${author.username}`}>
-          <a className="text-xs text-primary-600">{`@${author.username}`}</a>
-        </Link>
-      </div>
-    </div>
   )
 }
