50f022ef6079f1fff7043f78791c6712101707fb
behnam <salehinezhad.b@gmail.com>
2022-08-12 22:16:31 +0430

    add line

---
+++ components/Line.tsx
@@ -0,0 +1,3 @@
+export const Line = () => {
+  return <div className="h-[0.125rem] w-full bg-secondary-500"></div>
+}
--- components/Post.tsx
+++ components/Post.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import { Author } from '@/components/Author'
 import { CommentIcon } from '@/components/icons/CommentIcon'
 import { LikeIcon } from '@/components/icons/LikeIcon'
+import { Line } from '@/components/Line'
 import type { PostModel } from '@/models/post.model'
 import { dateTimeDistanceFilter } from '@/utils/date'
 
@@ -12,26 +13,29 @@ interface Props {
 
 export const Post = ({ post }: Props) => {
   return (
-    <article className="relative z-0 mb-4 border-b-2 border-secondary-500 pb-10 text-secondary-900 transition-colors duration-300 dark:text-primary-900">
-      <Link href={`/posts/${post.id}`}>
-        <div className="px-4">
-          <div className="flex items-center justify-between text-xs">
-            <Author id={post.owner_id} />
-            <div>{dateTimeDistanceFilter(post.created_at)}</div>
+    <>
+      <article className="relative z-0 mt-4 pb-10 text-secondary-900 transition-colors duration-300 dark:text-primary-900">
+        <Link href={`/posts/${post.id}`}>
+          <div className="px-4">
+            <div className="flex items-center justify-between text-xs">
+              <Author id={post.owner_id} />
+              <div>{dateTimeDistanceFilter(post.created_at)}</div>
+            </div>
+            <div className="my-1 ml-14">{post.text}</div>
           </div>
-          <div className="my-2 ml-14">{post.text}</div>
+        </Link>
+        <div className="absolute right-4 bottom-4 z-10 flex items-center justify-end">
+          <button className="ml-4">
+            <CommentIcon className="h-5 fill-secondary-900 transition-colors duration-300 dark:fill-primary-900" />
+          </button>
+          <div className="ml-2">{post.comments.length}</div>
+          <button className="ml-4">
+            <LikeIcon className="h-5 fill-secondary-900 transition-colors duration-300 dark:fill-primary-900" />
+          </button>
+          <div className="ml-2">{post.likes.length}</div>
         </div>
-      </Link>
-      <div className="absolute right-4 bottom-4 z-10 flex items-center justify-end">
-        <button className="ml-4">
-          <CommentIcon className="h-5 fill-secondary-900 transition-colors duration-300 dark:fill-primary-900" />
-        </button>
-        <div className="ml-2">{post.comments.length}</div>
-        <button className="ml-4">
-          <LikeIcon className="h-5 fill-secondary-900 transition-colors duration-300 dark:fill-primary-900" />
-        </button>
-        <div className="ml-2">{post.likes.length}</div>
-      </div>
-    </article>
+      </article>
+      <Line />
+    </>
   )
 }
