7e4ba354d0b5cff7017f219d227fa26aa04fc0c7
behnam <salehinezhad.b@gmail.com>
2022-08-12 05:35:04 +0430

    minor changes

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
 
-const Post: NextPage = ({}) => {
+const Post: NextPage = () => {
   const router = useRouter()
   const { id } = router.query
 
-  return <div>Post: {id}</div>
+  return (
+    <div className="text-secondary-900 dark:text-primary-900">Post: {id}</div>
+  )
 }
 
 export default Post
