069fedff2364e76fc5872fdf957a64465be2aa7e
behnam <salehinezhad.b@gmail.com>
2022-08-14 01:39:51 +0430

    add user comments

---
+++ components/User/Comments.tsx
@@ -0,0 +1,20 @@
+import { Comment } from '@/components/User/Comment'
+import type { CommentModel } from '@/models/comment.model'
+
+interface Props {
+  comments: [CommentModel]
+}
+
+export const Comments = ({ comments }: Props) => {
+  if (comments.length > 0) {
+    return (
+      <div>
+        {comments.map(comment => (
+          <Comment key={comment.id} comment={comment} />
+        ))}
+      </div>
+    )
+  } else {
+    return <div className="mt-8 text-center text-lg">No comments yet</div>
+  }
+}
