ac21796c0afade35a2e5534c2812b621284e9683
behnam <salehinezhad.b@gmail.com>
2022-08-16 00:57:20 +0430

    update ring color

--- components/Forms/TextInput.tsx
+++ components/Forms/TextInput.tsx
@@ -5,10 +5,10 @@ export const TextInput = (props: any) => {
   return (
     <div className="mb-4">
       <input
-        className={`mb-2 block w-full rounded-full bg-primary-200 py-3 px-4 text-sm text-primary-900 outline-none transition-colors duration-300 dark:bg-primary-600 dark:text-primary-100 ${
+        className={`mb-2 block w-full rounded-full bg-primary-200 py-3 px-4 text-sm text-primary-900 outline-none ring-2 transition-colors duration-300 dark:bg-primary-600 dark:text-primary-100 ${
           meta.touched && meta.error
-            ? 'ring-2 ring-red-600'
-            : 'focus:ring-2 focus:ring-blue-500'
+            ? 'ring-red-600'
+            : 'ring-primary-400 focus:ring-blue-500'
         }`}
         {...field}
         {...props}
