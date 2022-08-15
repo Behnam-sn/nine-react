4d593caf79de79faf7af10563716ca7c73d6d44c
behnam <salehinezhad.b@gmail.com>
2022-08-16 03:48:54 +0430

    change stroke

--- pages/notification.tsx
+++ pages/notification.tsx
@@ -5,7 +5,7 @@ import { BellIcon } from '@/components/icons/BellIcon'
 const Notification: NextPage = ({}) => {
   return (
     <main className="mt-52">
-      <BellIcon className="mx-auto h-32 w-32" />
+      <BellIcon className="mx-auto h-32 w-32 stroke-1" />
       <div className="mt-4 text-center text-xl">Nothing yet</div>
     </main>
   )
