2b091ffb5a6f0672c39d707ede96300fa38f7029
behnam <salehinezhad.b@gmail.com>
2022-08-16 03:42:45 +0430

    fix colors

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -9,23 +9,23 @@ export const NavBar = () => {
   return (
     <div className="fixed bottom-0 z-20 flex w-full justify-between bg-primary-100 p-3 transition-colors duration-300 dark:bg-primary-900 lg:hidden">
       <NavBarItem href="/">
-        <HomeIcon className="h-9 w-9 text-primary-900 transition-colors duration-300 dark:text-primary-100" />
+        <HomeIcon className="h-9 w-9" />
       </NavBarItem>
 
       <NavBarItem href="/search">
-        <SearchIcon className="h-9 w-9 text-primary-900 transition-colors duration-300 dark:text-primary-100" />
+        <SearchIcon className="h-9 w-9" />
       </NavBarItem>
 
       <NavBarItem href="/">
-        <PlusIcon className="h-9 w-9 text-primary-900 transition-colors duration-300 dark:text-primary-100" />
+        <PlusIcon className="h-9 w-9" />
       </NavBarItem>
 
       <NavBarItem href="/notification">
-        <BellIcon className="h-9 w-9 text-primary-900 transition-colors duration-300 dark:text-primary-100" />
+        <BellIcon className="h-9 w-9" />
       </NavBarItem>
 
       <NavBarItem href="/">
-        <UserIcon className="h-9 w-9 text-primary-900 transition-colors duration-300 dark:text-primary-100" />
+        <UserIcon className="h-9 w-9" />
       </NavBarItem>
     </div>
   )
