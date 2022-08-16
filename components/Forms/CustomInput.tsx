37c0c3e0c3af9ead04357d13bd66baecac28f478
behnam <salehinezhad.b@gmail.com>
2022-08-17 03:50:35 +0430

    minor change

--- components/Forms/CustomInput.tsx
+++ components/Forms/CustomInput.tsx
@@ -9,11 +9,12 @@ export const CustomInput = (props: any) => {
         className={`mb-2 block w-full rounded-full bg-primary-200 py-3 px-4 text-sm text-primary-900 outline-none ring-2 transition-colors duration-300 dark:bg-primary-600 dark:text-primary-100 ${
           meta.touched && meta.error
             ? 'ring-red-600'
-            : 'ring-primary-300 focus:ring-blue-500 dark:ring-primary-400'
+            : 'ring-primary-300 focus:ring-blue-500 dark:ring-primary-400 dark:focus:ring-blue-500'
         }`}
         {...field}
         {...props}
       />
+
       {meta.touched && meta.error ? (
         <div className="ml-4 text-sm text-red-600">{meta.error}</div>
       ) : null}
