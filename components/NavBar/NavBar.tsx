5f3f113ec30dc54f888b6ce85ec408dcd398a0c4
behnam <salehinezhad.b@gmail.com>
2022-08-16 05:13:53 +0430

    add active link

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,3 +1,5 @@
+import { useRouter } from 'next/router'
+
 import { BellIcon } from '@/components/icons/BellIcon'
 import { HomeIcon } from '@/components/icons/HomeIcon'
 import { PlusIcon } from '@/components/icons/PlusIcon'
@@ -5,27 +7,41 @@ import { SearchIcon } from '@/components/icons/SearchIcon'
 import { UserIcon } from '@/components/icons/UserIcon'
 import { NavBarItem } from '@/components/NavBar/NavBarItem'
 
+const activeLink = (url: string, pathname: string) => {
+  return pathname === url ? 'stroke-2' : ''
+}
+
 export const NavBar = () => {
+  const router = useRouter()
+
   return (
     <div className="fixed bottom-0 z-20 flex w-full justify-between bg-primary-100 p-3 transition-colors duration-300 dark:bg-primary-900 lg:hidden">
       <NavBarItem href="/">
-        <HomeIcon className="h-9 w-9" />
+        <HomeIcon className={`h-9 w-9 ${activeLink('/', router.pathname)}`} />
       </NavBarItem>
 
       <NavBarItem href="/search">
-        <SearchIcon className="h-9 w-9" />
+        <SearchIcon
+          className={`h-9 w-9 ${activeLink('/search', router.pathname)}`}
+        />
       </NavBarItem>
 
-      <NavBarItem href="/">
-        <PlusIcon className="h-9 w-9" />
+      <NavBarItem href="/add">
+        <PlusIcon
+          className={`h-9 w-9 ${activeLink('/add', router.pathname)}`}
+        />
       </NavBarItem>
 
       <NavBarItem href="/notification">
-        <BellIcon className="h-9 w-9" />
+        <BellIcon
+          className={`h-9 w-9 ${activeLink('/notification', router.pathname)}`}
+        />
       </NavBarItem>
 
-      <NavBarItem href="/">
-        <UserIcon className="h-9 w-9" />
+      <NavBarItem href="/profile">
+        <UserIcon
+          className={`h-9 w-9 ${activeLink('/profile', router.pathname)}`}
+        />
       </NavBarItem>
     </div>
   )
