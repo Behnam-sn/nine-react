dc4b84bbf6035ab52e6522ba6e0644d9dc2f8cc4
behnam <salehinezhad.b@gmail.com>
2022-08-19 03:44:23 +0430

    update follow style

--- components/User/User.tsx
+++ components/User/User.tsx
@@ -16,23 +16,23 @@ export const User = ({ user }: Props) => {
   return (
     <div className="px-4">
       <div className="my-2 flex content-center">
-        <div>
+        <div className="mr-4">
           <div className="flex h-24 w-24 items-center justify-center rounded-full bg-primary-900 transition-colors duration-300 dark:bg-primary-100">
             <UserCircleIcon className="h-20 w-20 text-primary-100 transition-colors duration-300 dark:text-primary-900" />
           </div>
         </div>
-        <div className="flex w-full">
-          <div className="flex w-full flex-col justify-center text-center">
+        <div className="flex w-full justify-around">
+          <div className="my-auto text-center">
             <div className="font-bold">{user.posts.length}</div>
-            <div>Posts</div>
+            <div className="text-sm font-light">Posts</div>
           </div>
-          <div className="flex w-full flex-col justify-center text-center">
+          <div className="my-auto text-center">
             <div className="font-bold">{user.followers.length}</div>
-            <div>Followers</div>
+            <div className="text-sm font-light">Followers</div>
           </div>
-          <div className="flex w-full flex-col justify-center text-center">
+          <div className="my-auto text-center">
             <div className="font-bold">{user.followings.length}</div>
-            <div>Followings</div>
+            <div className="text-sm font-light">Followings</div>
           </div>
         </div>
       </div>
