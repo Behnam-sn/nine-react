1c0f34acc45323c873292f3219f29d276f63fe5f
behnam <salehinezhad.b@gmail.com>
2022-08-21 03:06:37 +0430

    add auto signin

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,27 @@ import '@/styles/globals.css'
 
 import axios from 'axios'
 import type { AppProps } from 'next/app'
-import { SWRConfig } from 'swr'
+import { useEffect } from 'react'
+import { SWRConfig, useSWRConfig } from 'swr'
 
 import { FetchTheme } from '@/components/FetchTheme'
 import { HeaderBar } from '@/components/HeaderBar/HeaderBar'
 import { NavBar } from '@/components/NavBar/NavBar'
 import { ThemeProvider } from '@/contexts/ThemeContext'
+import { getCookie } from '@/utils/cookie'
 import { fetcher } from '@/utils/fetcher'
 
 axios.defaults.baseURL = 'http://127.0.0.1:8000/api/v1'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { mutate } = useSWRConfig()
+
+  useEffect(() => {
+    axios.defaults.headers.common['Authorization'] =
+      'Bearer ' + getCookie(document.cookie, 'token')
+    mutate('/users/current-user')
+  }, [])
+
   return (
     <>
       <SWRConfig
