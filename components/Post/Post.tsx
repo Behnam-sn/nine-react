eea7772371fda5c7d163b9527a06d53097d5fbe1
behnam <salehinezhad.b@gmail.com>
2022-08-21 15:20:21 +0430

    extract LikeButton

--- components/Post/Post.tsx
+++ components/Post/Post.tsx
@@ -1,16 +1,10 @@
-import axios from 'axios'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
-import { useSWRConfig } from 'swr'
 
 import { Author } from '@/components/Author'
 import { DateDistance } from '@/components/DateDistance'
 import { Divider } from '@/components/Divider'
 import { CommentIcon } from '@/components/icons/CommentIcon'
-import { OutlineHeartIcon } from '@/components/icons/OutlineHeartIcon'
-import { SolidHeartIcon } from '@/components/icons/SolidHeartIcon'
-import { useCurrentUser } from '@/hooks/useCurrentUser'
+import { LikeButton } from '@/components/Post/LikeButton'
 import type { PostModel } from '@/models/post.model'
 
 interface PostProps {
@@ -53,69 +47,5 @@ export const Post = ({ post }: PostProps) => {
 
       <Divider />
     </>
-  )
-}
-
-interface LikeButtonProps {
-  post: PostModel
-}
-
-export const LikeButton = ({ post }: LikeButtonProps) => {
-  const [isLiked, setIsLiked] = useState(false)
-  const { currentUser, loggedOut } = useCurrentUser()
-  const router = useRouter()
-  const { mutate } = useSWRConfig()
-
-  useEffect(() => {
-    if (currentUser) {
-      let found = post.likes.find(like => {
-        return like.owner_id === currentUser.id
-      })
-
-      found ? setIsLiked(true) : setIsLiked(false)
-    }
-  }, [currentUser, post.likes])
-
-  const handleClick = async () => {
-    if (loggedOut) {
-      router.push('/sign')
-    } else {
-      if (isLiked) {
-        await axios
-          .delete(`/likes/post/${post.id}`)
-          .then(() => {
-            setIsLiked(false)
-            mutate(`/posts/${post.id}`)
-            mutate('/users/current-user')
-          })
-          .catch(error => {
-            console.log(error)
-          })
-      } else {
-        await axios
-          .post(`/likes/post/${post.id}`)
-          .then(() => {
-            setIsLiked(true)
-            mutate(`/posts/${post.id}`)
-            mutate('/users/current-user')
-          })
-          .catch(error => {
-            console.log(error)
-          })
-      }
-    }
-  }
-
-  return (
-    <button
-      className="ml-4 rounded-full p-1 outline-none transition-colors duration-300 hover:bg-red-300/20 hover:text-red-500"
-      onClick={handleClick}
-    >
-      {isLiked ? (
-        <SolidHeartIcon className="h-6 w-6 text-red-500" />
-      ) : (
-        <OutlineHeartIcon className="h-6 w-6 stroke-2" />
-      )}
-    </button>
   )
 }
