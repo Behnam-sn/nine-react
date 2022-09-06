14b304513ffc64240c1f0a8cc97b27fba7bf8bb3
behnam <salehinezhad.b@gmail.com>
2022-09-06 22:00:19 +0430

    minor changes

--- hooks/useAuthor.tsx
+++ hooks/useAuthor.tsx
@@ -1,18 +1,18 @@
-import useSWR from 'swr'
+// import useSWR from 'swr'
 
-import type { AuthorModel } from '@/models/user.model'
+// import type { AuthorModel } from '@/models/user.model'
 
-interface useAuthorProps {
-  author: AuthorModel
-  isLoading: boolean
-  error: any
-}
+// interface useAuthorProps {
+//   author: AuthorModel
+//   isLoading: boolean
+//   error: any
+// }
 
-export const useAuthor = (id: number) => {
-  const { data, error } = useSWR(`/users/author/${id}`)
+// export const useAuthor = (id: number) => {
+//   const { data, error } = useSWR(`/users/author/${id}`)
 
-  return {
-    author: data,
-    isLoading: !data || error
-  } as useAuthorProps
-}
+//   return {
+//     author: data,
+//     isLoading: !data || error
+//   } as useAuthorProps
+// }
